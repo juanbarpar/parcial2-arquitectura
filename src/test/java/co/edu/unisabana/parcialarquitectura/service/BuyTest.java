bd377c576afc29f83f19882b025f21a7898e119f
juanbarpar <juanbarpar@unisabana.edu.co>
2022-10-04 08:35:09 -0500

    Implementacion de mocks en pruebas unitarias de clase Buy
    Se injectaron mocks en las pruebas unitarias para la clase Buy

--- src/test/java/co/edu/unisabana/parcialarquitectura/service/BuyTest.java
+++ src/test/java/co/edu/unisabana/parcialarquitectura/service/BuyTest.java
@@ -2,18 +2,19 @@ package co.edu.unisabana.parcialarquitectura.service;
 
 import co.edu.unisabana.parcialarquitectura.repository.Database;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class BuyTest {
 
-  private final Buy service = new Buy(new Database());
-
+  @InjectMocks
+  Buy service;
 
   @Test
   public void Given_same_vendor_and_buyer_When_makePurchase_Then_IllegalSaleException() {
-
+    service = new Buy(new Database());
     Throwable exception = assertThrows(IllegalSaleException.class, () -> {
       service.makePurchase(2, 2, "Books");
     });
@@ -22,20 +23,23 @@ class BuyTest {
   }
   @Test
   public void Given_a_vendor_and_a_different_buyer_When_makePurchase_Then_product_sold() {
-    service.makePurchase(2, 3, "Books");
+    service = new Buy(new Database());
     assertEquals("Product sold", service.makePurchase(2, 3, "Books"));
   }
 
   @Test
   public void Given_a_vendor_and_a_different_buyer_but_a_null_item_When_makePurchase_Then_sale_was_not_possible() {
+    service = new Buy(new Database());
     assertEquals("The sale was not possible", service.makePurchase(2, 3, null));
   }
   @Test
   public void Given_a_vendor_and_a_different_buyer_but_0_buyerCode_When_makePurchase_Then_sale_was_not_possible() {
+    service = new Buy(new Database());
     assertEquals("The sale was not possible", service.makePurchase(2, 0, "Books"));
   }
   @Test
   public void Given_a_vendor_and_a_different_buyer_but_item_is_empty_When_makePurchase_Then_sale_was_not_possible() {
+    service = new Buy(new Database());
     assertEquals("The sale was not possible", service.makePurchase(2, 3, ""));
   }
 
